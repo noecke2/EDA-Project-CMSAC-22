cc9135b961cc0518cb8dbf69ce15beaaf0d5824d
Andrew Noecker <noecke2@stolaf.edu>
2022-06-09 14:09:45 -0400

    Testing new commit/messages

--- trial_git_stuff.R
+++ trial_git_stuff.R
@@ -27,9 +27,23 @@ Batting%>%
   summarize_if(is.numeric, sum, na.rm = TRUE)%>%
   mutate(batting_avg = H/AB)
 
+# Testing
+# Testing testing testing?
 
 # Here's some other testing -----------------------------------------------
 
 year_batting_summary%>%
   ggplot(aes(x = HR, y = SO))+
   geom_point(alpha = 0.3)
+
+
+
+
+year_batting_summary%>%
+  ggplot(aes(x = HR, y = SO))+
+  geom_point(alpha = 0.3)+
+  labs(title = "this is a trial plot")+
+  theme_bw()
+
+
+# Here's some more test comments?
